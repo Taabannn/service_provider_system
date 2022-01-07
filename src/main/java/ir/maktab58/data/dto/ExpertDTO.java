12a4e61c4861fa9ca607f47eecb48ffa09cdef95
unknown <tabansoleymani@yahoo.com>
2022-01-06 22:58:48 -0800

    add ExpertDTO and CustomerDTO

--- src/main/java/ir/maktab58/data/dto/ExpertDTO.java
+++ src/main/java/ir/maktab58/data/dto/ExpertDTO.java
@@ -1,7 +1,21 @@
 package ir.maktab58.data.dto;
 
+import ir.maktab58.data.models.enums.UserStatus;
+import ir.maktab58.data.models.services.SubService;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  * @author Taban Soleymani
  */
 public class ExpertDTO {
+    String username;
+    String Email;
+    long Credit;
+    double score;
+    UserStatus userStatus;
+    Date firstAccess;
+    List<SubService> subServices = new ArrayList<>();
 }
