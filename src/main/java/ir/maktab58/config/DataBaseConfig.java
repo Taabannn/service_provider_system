056dea91356c6b1a0447395e6202df2ac1eff669
unknown <tabansoleymani@yahoo.com>
2022-01-11 03:40:26 -0800

    modify databaseConfig (with jpaRepositories)

--- src/main/java/ir/maktab58/config/DataBaseConfig.java
+++ src/main/java/ir/maktab58/config/DataBaseConfig.java
@@ -1,41 +1,31 @@
 package ir.maktab58.config;
 
-import ir.maktab58.data.models.Address;
-import ir.maktab58.data.models.Comment;
-import ir.maktab58.data.models.Offer;
-import ir.maktab58.data.models.Order;
-import ir.maktab58.data.models.services.MainService;
-import ir.maktab58.data.models.services.SubService;
-import ir.maktab58.data.models.users.Customer;
-import ir.maktab58.data.models.users.Expert;
-import ir.maktab58.data.models.users.Manager;
-import ir.maktab58.data.models.users.User;
-import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
 /**
  * @author Taban Soleymani
  */
+@EnableJpaRepositories(basePackages = "ir.maktab58.data.dao")
 @PropertySource("classpath:database.properties")
 @org.springframework.context.annotation.Configuration
 @EnableTransactionManagement
 public class DataBaseConfig {
-
-    private SessionFactory sessionFactory;
+    @Autowired
+    private Environment environment;
 
     @Value("${db.driver}")
     String driver;
@@ -52,7 +42,46 @@ public class DataBaseConfig {
     @Value("${hibernate.hbm2ddl.auto}")
     String hbm2ddl;
 
-    @Bean("sessionFactory")
+    @Bean
+    DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty(driver));
+        dataSource.setUrl(environment.getProperty(url));
+        dataSource.setUsername(environment.getProperty(user));
+        dataSource.setPassword(environment.getProperty(pass));
+        return dataSource;
+    }
+
+    @Bean
+    LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManagerFactoryBean.setPackagesToScan("ir.maktab58.data.model");
+
+        entityManagerFactoryBean.setJpaProperties(hibernateProperties());
+
+        return entityManagerFactoryBean;
+    }
+
+    private Properties hibernateProperties(){
+        Properties jpaProperties = new Properties();
+        jpaProperties.put(dialect, environment.getRequiredProperty("hibernate.dialect"));
+        jpaProperties.put(hbm2ddl, environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        //jpaProperties.put("hibernate.ejb.naming_strategy", environment.getRequiredProperty("hibernate.ejb.naming_strategy"));
+        jpaProperties.put(sql, environment.getRequiredProperty("hibernate.show_sql"));
+        //jpaProperties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+        return jpaProperties;
+    }
+
+    @Bean
+    JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
+    /*@Bean("sessionFactory")
     public  SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             try {
@@ -92,5 +121,5 @@ public class DataBaseConfig {
         settings.put(Environment.SHOW_SQL, sql);
         settings.put(Environment.HBM2DDL_AUTO, hbm2ddl);
         return settings;
-    }
+    }*/
 }
