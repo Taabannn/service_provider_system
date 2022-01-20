9fbddfe4263afd06406b46fcad2c0e44f302c583
unknown <tabansoleymani@yahoo.com>
2022-01-20 07:36:22 -0800

    update MainService (by adding Builder)

--- src/main/java/ir/maktab58/data/dto/services/MainServiceDto.java
+++ src/main/java/ir/maktab58/data/dto/services/MainServiceDto.java
@@ -1,9 +1,6 @@
 package ir.maktab58.data.dto.services;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 /**
  * @author Taban Soleymani
@@ -12,6 +9,7 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Builder(setterPrefix = "with")
 public class MainServiceDto {
     private String field;
 }
