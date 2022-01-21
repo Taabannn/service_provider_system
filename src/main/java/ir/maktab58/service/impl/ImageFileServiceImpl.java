6a4c47fe9574debcbd22b6f0c4922f5f0c05c823
unknown <tabansoleymani@yahoo.com>
2022-01-21 10:49:17 -0800

    add ImageFileRepository/Service/ServiceImpl

---
+++ src/main/java/ir/maktab58/service/impl/ImageFileServiceImpl.java
@@ -0,0 +1,9 @@
+package ir.maktab58.service.impl;
+
+import ir.maktab58.service.interfaces.ImageFileService;
+
+/**
+ * @author Taban Soleymani
+ */
+public class ImageFileServiceImpl implements ImageFileService {
+}
