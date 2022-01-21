17540e441401965ca0581afb25f1ac511bc1912f
unknown <tabansoleymani@yahoo.com>
2022-01-21 15:06:03 -0800

    update AddressDto by using javax.validator

--- src/main/java/ir/maktab58/dto/AddressDto.java
+++ src/main/java/ir/maktab58/dto/AddressDto.java
@@ -2,6 +2,9 @@ package ir.maktab58.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 /**
  * @author Taban Soleymani
  */
@@ -11,9 +14,19 @@ import lombok.*;
 @ToString
 @Builder(setterPrefix = "with")
 public class AddressDto {
+    @NotBlank(message = "alley field should not be empty")
     private String alley;
+
+    @NotBlank(message = "street field should not be empty")
     private String street;
+
+    @NotBlank(message = "city field should not be empty")
     private String city;
+
+    @NotBlank(message = "county field should not be empty")
     private String county;
+
+    @Pattern(regexp = "\\d{10}", message = "postal-code length must be 10")
+    @NotBlank(message = "postalCode should not be empty")
     private String postalCode;
 }
