5288525881c76f8e6a44e2090f002bd5c06bde89
unknown <tabansoleymani@yahoo.com>
2022-01-21 15:35:18 -0800

    add ExpertSubServiceDto class

--- src/main/java/ir/maktab58/dto/ExpertSubServiceDto.java
+++ src/main/java/ir/maktab58/dto/ExpertSubServiceDto.java
@@ -1,7 +1,17 @@
 package ir.maktab58.dto;
 
+import ir.maktab58.dto.services.SubServiceDto;
+import ir.maktab58.dto.users.ExpertDto;
+
+import javax.validation.constraints.NotNull;
+
 /**
  * @author Taban Soleymani
  */
 public class ExpertSubServiceDto {
+    @NotNull(message = "subService should not be null")
+    private SubServiceDto subServiceDto;
+
+    @NotNull(message = "expert should not be null")
+    private ExpertDto expertDto;
 }
