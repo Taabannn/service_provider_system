82397f37b326a0569849133bd0865c63029a4fba
unknown <tabansoleymani@yahoo.com>
2022-01-21 14:59:06 -0800

    update CustomerDto super constructor by using javax.validation

--- src/main/java/ir/maktab58/dto/users/CustomerDto.java
+++ src/main/java/ir/maktab58/dto/users/CustomerDto.java
@@ -1,10 +1,16 @@
 package ir.maktab58.dto.users;
 
 import ir.maktab58.dto.AddressDto;
+import ir.maktab58.dto.CustomerAddressDto;
 import ir.maktab58.dto.WalletDto;
 import ir.maktab58.data.enums.UserStatus;
+import ir.maktab58.service.validation.OnLogin;
+import ir.maktab58.service.validation.OnRegister;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.*;
 
 /**
@@ -16,14 +22,16 @@ import java.util.*;
 @ToString
 public class CustomerDto extends UserDto {
     private UserStatus userStatus;
+
     private WalletDto wallet;
-    private Set<AddressDto> addressDtoList = new HashSet<>();
+
+    private Set<CustomerAddressDto> customerAddressDtoSet = new HashSet<>();
 
     @Builder(setterPrefix = "with")
-    public CustomerDto(String firstName, String lastName, String username, String password, String email, Date firstAccess, Date lastUpdate, UserStatus userStatus, WalletDto wallet, Set<AddressDto> addressDtoList) {
+    public CustomerDto(@Size(min = 2, max = 20, message = "firstName should consist of 2-20 characters", groups = OnRegister.class) String firstName, @Size(min = 2, max = 20, message = "lastName should consist of 2-20 characters", groups = OnRegister.class) String lastName, @NotBlank(message = "username should not be empty", groups = OnLogin.class) @Pattern(regexp = "^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$", message = "username length must be between 3-18", groups = OnRegister.class) String username, @NotBlank(message = "password should not be empty", groups = OnLogin.class) @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,20}$", message = "password length must be between 8-20", groups = OnRegister.class) String password, @NotBlank(message = "email should not be empty", groups = OnLogin.class) @Pattern(regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", message = "email should be like xxxxx@example.com", groups = OnRegister.class) String email, Date firstAccess, Date lastUpdate, UserStatus userStatus, WalletDto wallet, Set<CustomerAddressDto> customerAddressDtoSet) {
         super(firstName, lastName, username, password, email, firstAccess, lastUpdate);
         this.userStatus = userStatus;
         this.wallet = wallet;
-        this.addressDtoList = addressDtoList;
+        this.customerAddressDtoSet = customerAddressDtoSet;
     }
 }
