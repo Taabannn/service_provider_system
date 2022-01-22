53da86a3d1758f3f1532ee41868643fd1789df68
unknown <tabansoleymani@yahoo.com>
2022-01-21 16:09:47 -0800

    update TransactionDto

--- src/main/java/ir/maktab58/dto/TransactionDto.java
+++ src/main/java/ir/maktab58/dto/TransactionDto.java
@@ -1,14 +1,26 @@
 package ir.maktab58.dto;
 
 import ir.maktab58.data.enums.TransactionStatus;
+import lombok.*;
 
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 /**
  * @author Taban Soleymani
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder(setterPrefix = "with")
 public class TransactionDto {
-    private OrderDto order;
+    @NotNull(message = "wallet could not be null")
+    private WalletDto walletDto;
+
     private Date creationDate;
+
     private TransactionStatus transactionStatus;
+
+    private long trackingCode;
 }
