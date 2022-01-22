36644a1807bd11d9eb5300657124640b19a226d3
unknown <tabansoleymani@yahoo.com>
2022-01-21 16:44:38 -0800

    add Mapper Annotation in MapperInterfaces

--- src/main/java/ir/maktab58/service/mapper/interfaces/ExpertSubServiceMapper.java
+++ src/main/java/ir/maktab58/service/mapper/interfaces/ExpertSubServiceMapper.java
@@ -2,10 +2,12 @@ package ir.maktab58.service.mapper.interfaces;
 
 import ir.maktab58.data.entities.ExpertSubService;
 import ir.maktab58.dto.ExpertSubServiceDto;
+import org.mapstruct.Mapper;
 
 /**
  * @author Taban Soleymani
  */
+@Mapper
 public interface ExpertSubServiceMapper {
     ExpertSubService toExpertSubService(ExpertSubServiceDto expertSubServiceDto);
 
--- src/main/java/ir/maktab58/service/mapper/interfaces/ImageFileMapper.java
+++ src/main/java/ir/maktab58/service/mapper/interfaces/ImageFileMapper.java
@@ -2,10 +2,12 @@ package ir.maktab58.service.mapper.interfaces;
 
 import ir.maktab58.data.entities.ImageFile;
 import ir.maktab58.dto.ImageFileDto;
+import org.mapstruct.Mapper;
 
 /**
  * @author Taban Soleymani
  */
+@Mapper
 public interface ImageFileMapper {
     ImageFile toImageFile(ImageFileDto imageFileDto);
 
