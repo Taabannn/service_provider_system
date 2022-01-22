6d43d65645b38fd29e49e3c67e1595c0c2889099
unknown <tabansoleymani@yahoo.com>
2022-01-22 07:28:53 -0800

    update ExpertSubServiceMapperImpl

--- src/main/java/ir/maktab58/dto/ExpertSubServiceDto.java
+++ src/main/java/ir/maktab58/dto/ExpertSubServiceDto.java
@@ -2,16 +2,25 @@ package ir.maktab58.dto;
 
 import ir.maktab58.dto.services.SubServiceDto;
 import ir.maktab58.dto.users.ExpertDto;
+import lombok.*;
 
 import javax.validation.constraints.NotNull;
+import java.util.Date;
 
 /**
  * @author Taban Soleymani
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder(setterPrefix = "with")
 public class ExpertSubServiceDto {
     @NotNull(message = "subService should not be null")
     private SubServiceDto subServiceDto;
 
     @NotNull(message = "expert should not be null")
     private ExpertDto expertDto;
+
+    private Date creationDate;
 }
--- src/main/java/ir/maktab58/service/mapper/Impl/ExpertSubServiceMapperImpl.java
+++ src/main/java/ir/maktab58/service/mapper/Impl/ExpertSubServiceMapperImpl.java
@@ -3,6 +3,7 @@ package ir.maktab58.service.mapper.Impl;
 import ir.maktab58.data.entities.ExpertSubService;
 import ir.maktab58.dto.ExpertSubServiceDto;
 import ir.maktab58.service.mapper.interfaces.ExpertSubServiceMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,13 +11,25 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class ExpertSubServiceMapperImpl implements ExpertSubServiceMapper {
+    @Autowired
+    private ExpertMapperImpl expertMapper;
+
+    @Autowired
+    private SubServiceMapperImpl subServiceMapper;
+
     @Override
     public ExpertSubService toExpertSubService(ExpertSubServiceDto expertSubServiceDto) {
-        return null;
+        return ExpertSubService.builder()
+                .withExpert(expertMapper.toExpert(expertSubServiceDto.getExpertDto()))
+                .withSubService(subServiceMapper.toSubService(expertSubServiceDto.getSubServiceDto()))
+                .withCreationDate(expertSubServiceDto.getCreationDate()).build();
     }
 
     @Override
     public ExpertSubServiceDto toExpertServiceDto(ExpertSubService expertSubService) {
-        return null;
+        return ExpertSubServiceDto.builder()
+                .withExpertDto(expertMapper.toExpertDto(expertSubService.getExpert()))
+                .withCreationDate(expertSubService.getCreationDate())
+                .withSubServiceDto(subServiceMapper.toSubServiceDto(expertSubService.getSubService())).build();
     }
 }
