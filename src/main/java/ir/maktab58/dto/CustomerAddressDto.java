ccc9dc21c691d05bbb57484187646d44179fbb70
unknown <tabansoleymani@yahoo.com>
2022-01-21 17:08:40 -0800

    update CustomerAddressMapperImpl

--- src/main/java/ir/maktab58/dto/CustomerAddressDto.java
+++ src/main/java/ir/maktab58/dto/CustomerAddressDto.java
@@ -1,12 +1,18 @@
 package ir.maktab58.dto;
 
 import ir.maktab58.dto.users.CustomerDto;
+import lombok.*;
 
 import javax.validation.constraints.NotNull;
 
 /**
  * @author Taban Soleymani
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder(setterPrefix = "with")
 public class CustomerAddressDto {
     @NotNull(message = "customer should not be null")
     private CustomerDto customerDto;
--- src/main/java/ir/maktab58/service/mapper/Impl/CustomerAddressMapperImpl.java
+++ src/main/java/ir/maktab58/service/mapper/Impl/CustomerAddressMapperImpl.java
@@ -2,19 +2,34 @@ package ir.maktab58.service.mapper.Impl;
 
 import ir.maktab58.data.entities.CustomerAddress;
 import ir.maktab58.dto.CustomerAddressDto;
+import ir.maktab58.service.mapper.interfaces.AddressMapper;
 import ir.maktab58.service.mapper.interfaces.CustomerAddressMapper;
+import ir.maktab58.service.mapper.interfaces.CustomerMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Taban Soleymani
  */
+@Component
 public class CustomerAddressMapperImpl implements CustomerAddressMapper {
+    @Autowired
+    private CustomerMapper customerMapper;
+
+    @Autowired
+    private AddressMapper addressMapper;
+
     @Override
     public CustomerAddress toCustomerAddress(CustomerAddressDto customerAddressDto) {
-        return null;
+        return CustomerAddress.builder()
+                .withCustomer(customerMapper.toCustomer(customerAddressDto.getCustomerDto()))
+                .withAddress(addressMapper.toAddress(customerAddressDto.getAddressDto())).build();
     }
 
     @Override
     public CustomerAddressDto toCustomerAddressDto(CustomerAddress customerAddress) {
-        return null;
+        return CustomerAddressDto.builder()
+                .withCustomerDto(customerMapper.toCustomerDto(customerAddress.getCustomer()))
+                .withAddressDto(addressMapper.toAddressDto(customerAddress.getAddress())).build();
     }
 }
