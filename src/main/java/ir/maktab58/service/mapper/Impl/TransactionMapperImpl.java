f86048008a462063ec5de60fda0323aef4a29195
unknown <tabansoleymani@yahoo.com>
2022-01-22 13:45:00 -0800

    update TransactionMapperImpl

--- src/main/java/ir/maktab58/service/mapper/Impl/TransactionMapperImpl.java
+++ src/main/java/ir/maktab58/service/mapper/Impl/TransactionMapperImpl.java
@@ -3,6 +3,7 @@ package ir.maktab58.service.mapper.Impl;
 import ir.maktab58.data.entities.Transaction;
 import ir.maktab58.dto.TransactionDto;
 import ir.maktab58.service.mapper.interfaces.TransactionMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,13 +11,28 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class TransactionMapperImpl implements TransactionMapper {
+    @Autowired
+    private WalletMapperImpl walletMapper;
+
     @Override
     public Transaction toTransaction(TransactionDto transactionDto) {
-        return null;
+        return Transaction.builder()
+                .withCost(transactionDto.getCost())
+                .withWallet(walletMapper.toWallet(transactionDto.getWalletDto()))
+                .withCreationDate(transactionDto.getCreationDate())
+                .withTransactionStatus(transactionDto.getTransactionStatus())
+                .withTransactionType(transactionDto.getTransactionType())
+                .withTrackingCode(transactionDto.getTrackingCode()).build();
     }
 
     @Override
     public TransactionDto toTransactionDto(Transaction transaction) {
-        return null;
+        return TransactionDto.builder()
+                .withCost(transaction.getCost())
+                .withWalletDto(walletMapper.toWalletDto(transaction.getWallet()))
+                .withCreationDate(transaction.getCreationDate())
+                .withTransactionStatus(transaction.getTransactionStatus())
+                .withTransactionType(transaction.getTransactionType())
+                .withTrackingCode(transaction.getTrackingCode()).build();
     }
 }
